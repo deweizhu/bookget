fa368e3df25780595fdab905f1b5ac98d1791668
zhudw <zhudwi@outlook.com>
2025-05-08 19:38:08 +0800

    update version

--- config/constant.go
+++ config/constant.go
@@ -7,14 +7,14 @@ import (
 )
 
 const (
-	Version              = "25.0507"
+	Version              = "25.0508"
 	CatalogVersionInfo   = "#版本=1.0" // 书签目录版本TXT
 	defaultUserAgent     = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
 	defaultFileExtension = ".jpg"
 
 	defaultRetry   = 3
 	defaultTimeout = 300 * time.Second
-	defaultQuality = 90
+	defaultQuality = 80
 	defaultFormat  = "full/full/0/default.jpg"
 )
 
