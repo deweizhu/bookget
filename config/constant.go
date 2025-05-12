d5035c0cd7824b33b6be67a8d5bc587e0e78d704
zhudw <zhudwi@outlook.com>
2025-05-12 13:11:16 +0800

    25.0512

--- config/constant.go
+++ config/constant.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	Version              = "25.0508"
+	Version              = "25.0512"
 	CatalogVersionInfo   = "#版本=1.0" // 书签目录版本TXT
 	defaultUserAgent     = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
 	defaultFileExtension = ".jpg"
